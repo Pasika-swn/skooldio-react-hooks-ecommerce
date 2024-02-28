39b2aaccf2e63a750153983ecd6bc447ceb9475a
Pasika Sriwanna <pasika.swn@gmail.com>
2024-02-28 22:22:01 +0700

    fetch API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,25 @@
+import { useEffect, useState } from 'react';
 import Hero from '../components/Hero';
 import ProductList from '../components/ProductList';
 
 import { products } from '../data';
 
-export const Home = () => (
-  <div>
-    <Hero />
-    <ProductList data={products} />
-  </div>
-);
+export const Home = () => {
+
+  useEffect(() => {
+    fetch('https://us-central1-skooldio-react-hooks.cloudfunctions.net/products')
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log("data",data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Hero />
+      <ProductList data={products} />
+    </div>
+  );
+};
 
 export default Home;
