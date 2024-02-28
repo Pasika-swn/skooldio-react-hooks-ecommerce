b90f05145b8f23c50ba2811f4bd49bcddb4b6089
Pasika Sriwanna <pasika.swn@gmail.com>
2024-02-28 22:32:10 +0700

    fetching API [loading]

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,37 @@ import { useEffect, useState } from 'react';
 import Hero from '../components/Hero';
 import ProductList from '../components/ProductList';
 
-import { products } from '../data';
+// import { products } from '../data';
 
 export const Home = () => {
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
+    setLoading(true)
     fetch('https://us-central1-skooldio-react-hooks.cloudfunctions.net/products')
       .then((resp) => resp.json())
       .then((data) => {
-        console.log("data",data);
+        // console.log("data",data);
+        setProducts(data)
+        setLoading(false)
       });
   }, []);
+
+  // if(loading) {
+  //   return <span style={{fontSize:}}>Loading...</span>
+  // }
 
   return (
     <div>
       <Hero />
-      <ProductList data={products} />
+      {
+        loading && <div style={{fontSize:"100px"}}>Loading</div>
+      }
+      {
+        !loading && <ProductList data={products} />
+      }
+      
     </div>
   );
 };
