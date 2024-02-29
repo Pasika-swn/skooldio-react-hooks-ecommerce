8c5b13791768dd2e719ed5b4740d6da837e83342
Pasika Sriwanna <pasika.swn@gmail.com>
2024-02-29 09:22:54 +0700

    fetch API [productDetail] && add loading spinner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,29 @@
 import { useEffect, useState } from 'react';
 import Hero from '../components/Hero';
 import ProductList from '../components/ProductList';
+import { ColorRing } from 'react-loader-spinner';
+import styled from 'styled-components';
 
 // import { products } from '../data';
+const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
 
 export const Home = () => {
-  const [products, setProducts] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     fetch('https://us-central1-skooldio-react-hooks.cloudfunctions.net/products')
       .then((resp) => resp.json())
       .then((data) => {
         // console.log("data",data);
-        setProducts(data)
-        setLoading(false)
+        setProducts(data);
+        setLoading(false);
       });
   }, []);
 
@@ -26,13 +34,20 @@ export const Home = () => {
   return (
     <div>
       <Hero />
-      {
-        loading && <div style={{fontSize:"100px"}}>Loading</div>
-      }
-      {
-        !loading && <ProductList data={products} />
-      }
-      
+      {loading && (
+        <LoadingContainer>
+          <ColorRing
+            visible={true}
+            height="180"
+            width="180"
+            ariaLabel="color-ring-loading"
+            wrapperStyle={{}}
+            wrapperClass="color-ring-wrapper"
+            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+          />
+        </LoadingContainer>
+      )}
+      {!loading && <ProductList data={products} />}
     </div>
   );
 };
--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -7,51 +7,62 @@ import Input from '../components/Input';
 import { numberWithCommas } from '../utils';
 import { products } from '../data';
 
-const Container = styled(BaseContainer)`
-  padding-top: 78px;
-  padding-bottom: 78px;
-  display: grid;
-  grid-template-columns: 1fr;
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { ColorRing } from 'react-loader-spinner';
 
-  @media screen and (min-width: 768px) {
-    grid-template-columns: 1fr 1fr;
-    gap: 96px;
-  }
+const Container = styled(BaseContainer)`
+ padding-top: 78px;
+ padding-bottom: 78px;
+ display: grid;
+ grid-template-columns: 1fr;
+
+ @media screen and (min-width: 768px) {
+  grid-template-columns: 1fr 1fr;
+  gap: 96px;
+ }
 `;
 
 const ProductImage = styled.img`
-  width: 100%;
+ width: 100%;
 `;
 
 const ProductInfo = styled.div`
-  display: flex;
-  flex-direction: column;
+ display: flex;
+ flex-direction: column;
 `;
 
 const Title = styled.div`
-  font-size: 40px;
-  line-height: 60px;
-  font-weight: 600;
-  margin-bottom: 72px;
+ font-size: 40px;
+ line-height: 60px;
+ font-weight: 600;
+ margin-bottom: 72px;
 `;
 
 const Subtitle = styled.div`
-  display: flex;
-  justify-content: space-between;
-  font-size: 32px;
-  line-height: 48px;
-  font-weight: 400;
-  color: #000000;
+ display: flex;
+ justify-content: space-between;
+ font-size: 32px;
+ line-height: 48px;
+ font-weight: 400;
+ color: #000000;
 `;
 
 const Description = styled.p`
-  font-size: 24px;
-  line-height: 36px;
-  font-weight: 300;
+ font-size: 24px;
+ line-height: 36px;
+ font-weight: 300;
 
-  color: #000000;
-  margin-top: 0;
-  margin-bottom: 72px;
+ color: #000000;
+ margin-top: 0;
+ margin-bottom: 72px;
+`;
+
+const LoadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 `;
 
 const data = products[0];
@@ -59,20 +70,53 @@ const data = products[0];
 /**
  * Below is the main ProductDetail component.
  */
-export const ProductDetail = () => (
+export const ProductDetail = () => {
+ const { productId } = useParams();
+
+ const [data, setProduct] = useState(null);
+ const [loading, setLoading] = useState(false);
+
+ useEffect(() => {
+  setLoading(true);
+  fetch('https://us-central1-skooldio-react-hooks.cloudfunctions.net/products/' + productId)
+   .then((resp) => resp.json())
+   .then((data) => {
+    setProduct(data);
+    setLoading(false);
+   });
+ }, []);
+
+ if (loading || !data) {
+  return (
+   <LoadingContainer>
+     <ColorRing
+      visible={true}
+      height="180"
+      width="180"
+      ariaLabel="color-ring-loading"
+      wrapperStyle={{}}
+      wrapperClass="color-ring-wrapper"
+      colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+      />
+   </LoadingContainer>
+  );
+ }
+
+ return (
   <Container>
-    <ProductImage src={data.imageUrl} alt={`${data.name}`} />
-    <ProductInfo>
-      <Subtitle>
-        <span>{data.category}</span>
-        <span>฿{numberWithCommas(data.price)}</span>
-      </Subtitle>
-      <Title>{data.name}</Title>
-      <Description>{data.description}</Description>
-      <Input style={{ marginBottom: '40px' }} type={'number'} label={'Quantity'} />
-      <Button>Add to Cart</Button>
-    </ProductInfo>
+   <ProductImage src={data.imageUrl} alt={`${data.name}`} />
+   <ProductInfo>
+    <Subtitle>
+     <span>{data.category}</span>
+     <span>฿{numberWithCommas(data.price)}</span>
+    </Subtitle>
+    <Title>{data.name}</Title>
+    <Description>{data.description}</Description>
+    <Input style={{ marginBottom: '40px' }} type={'number'} label={'Quantity'} />
+    <Button>Add to Cart</Button>
+   </ProductInfo>
   </Container>
-);
+ );
+};
 
 export default ProductDetail;
